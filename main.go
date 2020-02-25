0e21ad93236eaefd50b16d01400cdb978e0b4d91
Deiklov <romanov408g@mail.ru>
2020-02-25 10:49:18 +0300

    Разделил на пакеты

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
+	"./routes"
 	"html/template"
 	"net/http"
 )
@@ -10,12 +10,8 @@ var templates *template.Template
 
 func main() {
 	templates = template.Must(template.ParseGlob("public/*.html"))
-	r := mux.NewRouter()
-
-	r.HandleFunc("/new/repository", NewRepository).Methods("POST")
+	r := routes.NewRouter()
 	r.HandleFunc("/", indexHandler).Methods("GET")
-	fs := http.FileServer(http.Dir("./public"))
-	r.PathPrefix("/public/").Handler(http.StripPrefix("/public", fs))
 	http.Handle("/", r)
 	http.ListenAndServe(":8080", r)
 }
