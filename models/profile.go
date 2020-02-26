5464d699f98ee46bb4bf1d5861e591b828572a60
AntonElagin <anton120299@gmail.com>
2020-02-26 06:09:54 +0300

    cahnge updateProfile and add loadFile

--- models/profile.go
+++ models/profile.go
@@ -1,11 +1,15 @@
 package models
 
 import (
+	"crypto/md5"
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+
+	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"strings"
 )
 
@@ -13,14 +17,15 @@ var profilMap = map[string]Profile{
 	"antonelagin": Profile{
 		Login:     "AntonElagin",
 		Avatar:    "https://avatars.mds.yandex.net/get-pdb/1964870/cfaa9b42-d74b-40f7-93cc-1d777bb5654c/s1200",
-		RegDate:   "12.02.1999",
+		Name:      "ANton ELagin",
+		Bio:       "kek lol opps, long sfddsfdsfd dfdsfs dsfds sdfdsf sdfsd sdfd",
+		URL:       "google.com",
 		Followers: 1,
 		Following: 2,
 	},
 	"keklol": Profile{
 		Login:     "keeeekLooool",
 		Avatar:    "https://avatars.mds.yandex.net/get-pdb/1964870/cfaa9b42-d74b-40f7-93cc-1d777bb5654c/s1200",
-		RegDate:   "12.02.1999",
 		Followers: 1,
 		Following: 150,
 	}}
@@ -31,14 +36,19 @@ type Result struct {
 }
 
 type Profile struct {
-	Login     string `json:"login"`
 	Avatar    string `json:"avatar"`
-	RegDate   string `json:"registrationDate"`
+	Login     string `json:"login"`
+	Name      string `json:"name"`
+	Bio       string `json:"Bio"`
+	URL       string `json:"url"`
 	Followers uint   `json:"followers"`
 	Following uint   `json:"following"`
 }
 
 func UpdateProfile(w http.ResponseWriter, r *http.Request) {
+
+	// TODO : проверка авторизации
+
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Content-Type", "application/json")
 	prof := Profile{}
@@ -58,8 +68,17 @@ func UpdateProfile(w http.ResponseWriter, r *http.Request) {
 		})
 		return
 	}
+	login := strings.ToLower(prof.Login)
+	if _, ok := profilMap[login]; !ok {
+		json.NewEncoder(w).Encode(&Result{
+			Err: "unknown login",
+		})
+		return
+	}
+	profilMap[login] = prof
 
-	json.NewEncoder(w).Encode(&Result{Body: prof})
+	json.NewEncoder(w).Encode(&Result{Body: "success"})
+
 }
 
 func GetProfile(w http.ResponseWriter, r *http.Request) {
@@ -67,7 +86,7 @@ func GetProfile(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	vars := mux.Vars(r)
 	login := vars["login"]
-	fmt.Println(login, strings.ToLower(login))
+
 	profile, ok := profilMap[strings.ToLower(login)]
 	if !ok {
 		json.NewEncoder(w).Encode(&Result{
@@ -76,4 +95,45 @@ func GetProfile(w http.ResponseWriter, r *http.Request) {
 	}
 
 	json.NewEncoder(w).Encode(&Result{Body: profile})
+}
+
+// не работает
+func UploadAvatar(w http.ResponseWriter, r *http.Request) {
+	r.ParseMultipartForm(5 * 1024 * 1025)
+	file, handler, err := r.FormFile("avatar")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	fmt.Fprintf(w, "handler.Filename %v\n", handler.Filename)
+	fmt.Fprintf(w, "handler.Header %#v\n", handler.Header)
+
+	hasher := md5.New()
+	io.Copy(hasher, file)
+
+	filePath := "./static/image/" + handler.Filename
+	img, errImg := os.Create(filePath)
+	if errImg != nil {
+		json.NewEncoder(w).Encode(&Result{
+			Err: "Server Error",
+		})
+		// 	return
+	}
+	defer img.Close()
+
+	io.Copy(img, file)
+	elem, er := profilMap["antonelagin"]
+	if !er {
+		json.NewEncoder(w).Encode(&Result{
+			Err: "unknown login",
+		})
+		return
+	}
+
+	elem.Avatar = "http://localhost:8080/static/image/" + handler.Filename
+	profilMap["antonelagin"] = elem
+
+	fmt.Fprintf(w, "md5 %x\n", hasher.Sum(nil))
 }
--- routes/routes.go
+++ routes/routes.go
@@ -10,12 +10,22 @@ func NewRouter() *mux.Router {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/new/repository", models.NewRepository).Methods("POST")
-	r.HandleFunc("/settings/profile", models.UpdateProfile).Methods("PUT")
+	r.HandleFunc("/settings/profile", models.UpdateProfile).Methods("POST")
 	r.HandleFunc("/profile/{login}", models.GetProfile).Methods("GET")
 	r.HandleFunc("/repository/{login}", models.GetRepositoryList).Methods("GET")
+	r.HandleFunc("/settings/avatar", models.UploadAvatar).Methods("POST")
 
 	fs := http.FileServer(http.Dir("./public"))
 	r.PathPrefix("/public/").Handler(http.StripPrefix("/public", fs))
+
+	staticHandler := http.FileServer(http.Dir("./static"))
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static", staticHandler))
+
+	// staticHandler := http.StripPrefix(
+	// 	"/data/",
+	// 	http.FileServer(http.Dir("./static")),
+	// )
+	// http.Handle("/data/", staticHandler)
 
 	return r
 }
