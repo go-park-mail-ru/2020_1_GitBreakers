aaa72ebfff7ef2bd474527e52e8e054dc849b2b5
Nicholas Eskov <mr.eskov1@yandex.ru>
2020-04-01 02:32:24 +0300

    updated database.sql script

--- internal/database.sql
+++ internal/database.sql
@@ -1,66 +1,23 @@
-create table if not exists users
+-- Main
+
+CREATE TABLE IF NOT EXISTS users
 (
-    id serial not null
-        constraint users_pk
-            primary key,
-    email text not null,
-    password text not null,
-    login text not null,
-    name text,
-    avatar_path text,
-    created_at timestamp default CURRENT_TIMESTAMP not null,
-    email_verified boolean default false not null
+    id             SERIAL PRIMARY KEY                  NOT NULL UNIQUE,
+    login          VARCHAR(128)                        NOT NULL UNIQUE,
+    email          VARCHAR(128)                        NOT NULL UNIQUE,
+    name           VARCHAR(256)                        NOT NULL DEFAULT '',
+    avatar_path    VARCHAR(1024)                       NOT NULL,
+    email_verified BOOLEAN   DEFAULT FALSE             NOT NULL,
+    created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
-
-create unique index if not exists users_email_uindex
-    on users (email);
-
-create unique index if not exists users_id_uindex
-    on users (id);
-
-create unique index if not exists users_login_uindex
-    on users (login);
-
-create table if not exists storage
+CREATE TABLE IF NOT EXISTS git_repository
 (
-    id serial not null
-        constraint storage_pk
-            primary key,
-    ownerid integer not null,
-    name text not null,
-    description text
+    id          SERIAL PRIMARY KEY NOT NULL UNIQUE,
+    ownerid     INTEGER            NOT NULL,
+    name        VARCHAR(512)       NOT NULL,
+    description VARCHAR(2048)      NOT NULL,
+
+    FOREIGN KEY (ownerid) REFERENCES users (id),
+    UNIQUE (id, ownerid)
 );
-
-
-create unique index if not exists storage_id_uindex
-    on storage (id);
-
-create table if not exists branches
-(
-    id serial not null
-        constraint branches_pk
-            primary key,
-    storageid integer not null,
-    commits integer default 0 not null
-);
-
-
-create unique index if not exists branches_id_uindex
-    on branches (id);
-
-create table if not exists commits
-(
-    id serial not null
-        constraint commits_pk
-            primary key,
-    description text not null,
-    hash text not null,
-    branchid integer not null,
-    date timestamp default CURRENT_TIMESTAMP not null
-);
-
-
-create unique index if not exists commits_id_uindex
-    on commits (id);
-
