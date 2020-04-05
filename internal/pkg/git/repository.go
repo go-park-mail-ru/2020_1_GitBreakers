1b81433ae0c5bd06abd0ff716b75366053dc0d92
Nicholas Eskov <mr.eskov1@yandex.ru>
2020-04-05 18:38:23 +0300

    implemented new methods of repository for git repositrofy

--- internal/pkg/git/repository.go
+++ internal/pkg/git/repository.go
@@ -5,16 +5,16 @@ import (
 )
 
 type Repository interface {
-	GetById(id int) (git.Repository, error)                       // DONE
-	GetByName(userLogin, repoName string) (git.Repository, error) // DONE
-	Create(repos git.Repository) (id int64, err error)            // DONE
+	GetById(id int) (git.Repository, error)
+	GetByName(userLogin, repoName string) (git.Repository, error)
+	Create(repos git.Repository) (id int64, err error)
 	//DeleteById(id int) error // TODO
 	//DeleteByName(userId int, repoName string) error // TODO
-	CheckReadAccess(currentUserId *int, userLogin, repoName string) (bool, error)                        // DONE
-	GetBranchesByName(userLogin, repoName string) ([]git.Branch, error)                                  // DONE
-	GetAnyReposByUserLogin(userLogin string, offset, limit int) ([]git.Repository, error)                // DONE
-	GetReposByUserLogin(requesterId *int, userLogin string, offset, limit int) ([]git.Repository, error) // DONE
+	CheckReadAccess(currentUserId *int, userLogin, repoName string) (bool, error)
+	GetBranchesByName(userLogin, repoName string) ([]git.Branch, error)
+	GetAnyReposByUserLogin(userLogin string, offset, limit int) ([]git.Repository, error)
+	GetReposByUserLogin(requesterId *int, userLogin string, offset, limit int) ([]git.Repository, error)
 
-	//GetCommitsInBranch(userLogin, repoName, branchName string, offset, limit int) ([]git.Commit, error) // TODO
-	//FilesInCommitByPath(userLogin, repoName, commitHash, path string) ([]git.FileInCommit, error)       // TODO
+	FilesInCommitByPath(userLogin, repoName, commitHash, path string) ([]git.FileInCommit, error)
+	GetCommitsByCommitHash(userLogin, repoName, commitHash string, offset, limit int) ([]git.Commit, error)
 }
