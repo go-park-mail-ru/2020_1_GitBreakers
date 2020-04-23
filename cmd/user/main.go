f37f083c0e0873e7a42da4a9c9bf88fe399d8672
Deiklov <romanov408g@mail.ru>
2020-04-23 12:08:40 +0300

    user server without avatar

--- cmd/user/main.go
+++ cmd/user/main.go
@@ -1,5 +1,56 @@
 package main
 
-func main() {
+import (
+	"github.com/asaskevich/govalidator"
+	"github.com/go-park-mail-ru/2020_1_GitBreakers/internal/config"
+	usergrpc "github.com/go-park-mail-ru/2020_1_GitBreakers/internal/pkg/user/delivery/grpc"
+	"github.com/go-park-mail-ru/2020_1_GitBreakers/internal/pkg/user/repository/postgres"
+	userUC "github.com/go-park-mail-ru/2020_1_GitBreakers/internal/pkg/user/usecase"
+	"github.com/jmoiron/sqlx"
+	"github.com/joho/godotenv"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
 
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+	govalidator.SetFieldsRequiredByDefault(true)
+}
+
+func main() {
+	s := grpc.NewServer()
+	conf := config.New()
+
+	connStr := "user=" + conf.POSTGRES_USER + " password=" +
+		conf.POSTGRES_PASS + " dbname=" + conf.POSTGRES_DBNAME
+
+	db, err := sqlx.Connect("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		log.Println("Database started success")
+	}
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	db.SetMaxOpenConns(conf.MAX_DB_OPEN_CONN) //10 по дефолту
+	userRepos := postgres.NewUserRepo(db, "default.jpg", "/static/image/avatar/", conf.HOST_TO_SAVE)
+	userUCase := userUC.UCUser{RepUser: &userRepos}
+
+	usergrpc.NewUserServer(s, &userUCase)
+
+	l, err := net.Listen("tcp", ":8082")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := s.Serve(l); err != nil {
+		log.Fatal(err)
+	}
 }
--- internal/pkg/user/delivery/grpc/grpc_user.go
+++ internal/pkg/user/delivery/grpc/grpc_user.go
@@ -2,6 +2,7 @@ package usergrpc
 
 import (
 	"context"
+	"github.com/go-park-mail-ru/2020_1_GitBreakers/internal/pkg/models"
 	"github.com/go-park-mail-ru/2020_1_GitBreakers/internal/pkg/user"
 	"github.com/golang/protobuf/ptypes/empty"
 	"google.golang.org/grpc"
@@ -18,21 +19,60 @@ func NewUserServer(gserver *grpc.Server, UserUseCase user.UCUser) {
 	reflection.Register(gserver)
 }
 func (h *UserServer) Create(ctx context.Context, in *UserModel) (*empty.Empty, error) {
-	return nil, nil
+	User := models.User{
+		ID:       int(in.GetId()),
+		Password: in.GetPassword(),
+		Name:     in.GetName(),
+		Login:    in.GetLogin(),
+		Image:    in.GetImage(),
+		Email:    in.GetEmail(),
+	}
+	return nil, h.UC.Create(User)
 }
 func (h *UserServer) GetByLogin(ctx context.Context, in *LoginModel) (*UserModel, error) {
-	return nil, nil
+	User, err := h.UC.GetByLogin(in.GetLogin())
+	userModel := h.transfToUserModelGRPC(&User)
+
+	return &userModel, err
 }
 func (h *UserServer) UpdateUser(ctx context.Context, in *UserUpdateModel) (*empty.Empty, error) {
-	return nil, nil
+	UserID := int(in.GetUserID())
+	User := h.transfToModelsUser(in.GetUserData())
+	return nil, h.UC.Update(UserID, User)
 }
 func (h *UserServer) CheckPass(ctx context.Context, in *CheckPassModel) (*CheckPassResp, error) {
-	return nil, nil
+	isCorrect, err := h.UC.CheckPass(in.GetLogin(), in.GetPass())
+	return &CheckPassResp{IsCorrect: isCorrect}, err
 }
 func (h *UserServer) GetByID(ctx context.Context, in *UserIDModel) (*UserModel, error) {
-	return nil, nil
+	User, err := h.UC.GetByID(int(in.GetUserID()))
+	userModel := h.transfToUserModelGRPC(&User)
+	return &userModel, err
 }
-//todo не хочет удовлетворять интерфейсу
-func (h *UserServer) UploadAvatar(ctx context.Context, in *UserUpdateModel) error {
+func (h *UserServer) UploadAvatar(UserGrpc_UploadAvatarServer) error {
 	return nil
+}
+
+//функции вспомогательные для преобразования from/to models.User UserModel(GRPC)
+func (h *UserServer) transfToModelsUser(in *UserModel) models.User {
+	return models.User{
+		ID:       int(in.GetId()),
+		Password: in.GetPassword(),
+		Name:     in.GetName(),
+		Login:    in.GetLogin(),
+		Image:    in.GetImage(),
+		Email:    in.GetEmail(),
+	}
+}
+
+//функции вспомогательные для преобразования from/to models.User UserModel(GRPC)
+func (h *UserServer) transfToUserModelGRPC(in *models.User) UserModel {
+	return UserModel{
+		Id:       int64(in.ID),
+		Password: in.Password,
+		Name:     in.Name,
+		Login:    in.Login,
+		Image:    in.Image,
+		Email:    in.Email,
+	}
 }
