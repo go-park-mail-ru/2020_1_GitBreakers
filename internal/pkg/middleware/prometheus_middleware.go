62f80d95f287b5ba4cccfa45d49c1fff76cf112f
Nicholas Eskov <mr.eskov1@yandex.ru>
2020-05-03 01:59:50 +0300

    changed path value in metrics to path template value and renamed middlewares

--- internal/pkg/middleware/prometheus_middleware.go
+++ internal/pkg/middleware/prometheus_middleware.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"github.com/go-park-mail-ru/2020_1_GitBreakers/internal/monitoring"
 	customHttp "github.com/go-park-mail-ru/2020_1_GitBreakers/pkg/http"
+	"github.com/gorilla/mux"
 	"github.com/prometheus/client_golang/prometheus"
 	"net/http"
 	"strconv"
@@ -11,18 +12,21 @@ import (
 func PrometheusMetricsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		newResponseWriter := &customHttp.ResponseWriter{ResponseWriter: w}
-		path := r.URL.Path
+		template, err := mux.CurrentRoute(r).GetPathTemplate()
+		if err != nil {
+			template = r.URL.Path
+		}
 
 		defer func() {
 			statusCode := newResponseWriter.GetStatusCode()
 			if statusCode == 0 {
 				statusCode = http.StatusOK
 			}
-			monitoring.Hits.WithLabelValues(strconv.Itoa(statusCode), path).Inc()
+			monitoring.Hits.WithLabelValues(strconv.Itoa(statusCode), template).Inc()
 		}()
 
 		timer := prometheus.NewTimer(monitoring.RequestDuration.With(
-			prometheus.Labels{"path": path, "method": r.Method},
+			prometheus.Labels{"path": template, "method": r.Method},
 		))
 		defer timer.ObserveDuration()
 
