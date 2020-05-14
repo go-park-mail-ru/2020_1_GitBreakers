0dbd73aa75786a8ddd9adf9c2e0eaff353c3a2f2
Nicholas Eskov <mr.eskov1@yandex.ru>
2020-05-14 06:09:38 +0300

    fixed bug in git repository model with govalidator and added errors types to entityerrors

--- internal/pkg/models/git/repository.go
+++ internal/pkg/models/git/repository.go
@@ -11,7 +11,7 @@ type Repository struct {
 	CreatedAt   time.Time `json:"created_at" valid:"-"`
 	IsPublic    bool      `json:"is_public" valid:"-"`
 	Stars       int64     `json:"stars" valid:"-"`
-	AuthorLogin string    `json:"author_login"`
+	AuthorLogin string    `json:"author_login" valid:"-"`
 }
 
 //easyjson:json
