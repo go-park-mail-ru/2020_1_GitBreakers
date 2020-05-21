77e3f059876c968234cb5243c0d0407d87521fc0
Nicholas Eskov <mr.eskov1@yandex.ru>
2020-05-21 17:38:48 +0300

    updated .gitignore and run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 killall main
 sleep 3
-nohup go run cmd/auth/main.go >> authservice_nohup.log &
+nohup go run cmd/auth/main.go >> authservice_nohup.out &
 sleep 1
-nohup go run cmd/user/main.go >> userservice_nohup.log &
+nohup go run cmd/user/main.go >> userservice_nohup.out &
 sleep 1
 nohup go run cmd/news/main.go >> newsservice_nohup.out &
 sleep 1
