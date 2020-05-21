f8ad91dda2964e3434689eecf77c93f40c05c7b4
Nicholas Eskov <mr.eskov1@yandex.ru>
2020-05-21 18:48:02 +0300

    updated run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-killall main
+killall -q main || echo "No running processes"
 sleep 3
-nohup go run cmd/auth/main.go >> authservice_nohup.out 2>&1 &
+nohup go run cmd/auth/main.go >>authservice_nohup.out 2>&1 &
 sleep 1
-nohup go run cmd/user/main.go >> userservice_nohup.out 2>&1 &
+nohup go run cmd/user/main.go >>userservice_nohup.out 2>&1 &
 sleep 1
-nohup go run cmd/news/main.go >> newsservice_nohup.out 2>&1 &
+nohup go run cmd/news/main.go >>newsservice_nohup.out 2>&1 &
 sleep 1
-nohup go run cmd/gitserver/main.go >> gitservice_logfile.log 2>&1 &
+nohup go run cmd/gitserver/main.go >>gitservice_logfile.log 2>&1 &
 sleep 1
-nohup go run cmd/server/main.go >> codehubserivce_nohup.out 2>&1 &
+nohup go run cmd/server/main.go >>codehubserivce_nohup.out 2>&1 &
